689db1b1287556e92b7e42d1ab43c46a3b5068b3
Oluwadunsindavid <oluwadunsindavid21@gmail.com>
2023-08-20 04:23:56 -0700

    _printf.c added

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void print_buffer(charbuffer[], int *buff_ind);
+
+/**
+ * _printf: Print a function
+ * @format: format
+ * Return: Printed chars
+ */
+int _printf(const char *format, ...)
+
+{
+	int i,printed = 0,printed char = 0;
+
